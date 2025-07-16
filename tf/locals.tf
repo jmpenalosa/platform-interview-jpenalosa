ce3ae1eed84a69cfa250057650c5bd9876216c32
Josh Penalosa <49530001+jmpenalosa@users.noreply.github.com>
2025-07-16 08:31:55 -0400

    Update locals.tf

--- tf/locals.tf
+++ tf/locals.tf
@@ -1,16 +1,19 @@
 locals {
   providers = {
     development = "vault.vault_dev"
+    stage       = "vault.vault_stage"
     production  = "vault.vault_prod"
   }
 
   vault_addrs = {
     development = "http://vault-development:8200"
+    stage       = "http://vault-stage:8200"
     production  = "http://vault-production:8200"
   }
 
   networks = {
     development = "vagrant_development"
+    stage       = "vagrant_stage"
     production  = "vagrant_production"
   }
 }
